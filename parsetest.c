3e1154814c286d73dcabfcba47d31f52a85a37c6
jfangwang <qbs1864@gmail.com>
2020-12-18 23:08:32 +0000

    parse can open, read, and printout the contents of the file given file path

--- parsetest.c
+++ parsetest.c
@@ -1,3 +1,18 @@
 #include "monty.h"
 
-
+#define MAXCHAR 1000
+int main() {
+    FILE *fp;
+    char str[MAXCHAR];
+    char* filename = "/home/vagrant/monty/tests/01.m";
+ 
+    fp = fopen(filename, "r");
+    if (fp == NULL){
+        printf("Could not open file %s",filename);
+        return 1;
+    }
+    while (fgets(str, MAXCHAR, fp) != NULL)
+        printf("%s", str);
+    fclose(fp);
+    return 0;
+}
---
+++ tests/01.m
@@ -0,0 +1,5 @@
+push 1
+push 2 #This is a comment
+   push 3 
+       push 4
+pall
