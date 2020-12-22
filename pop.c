0a85463a2e98c1ce2519ed68f39b7e32b9583358
nehpets <nehpets0701@gmail.com>
2020-12-22 15:09:40 +0000

    pop

---
+++ pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * pop-remove top node
+ * @line:line args came from
+ * @head:input
+ */
+void pop(unsigned int line, stack_t **head)
+{
+	stack_t *new = *head;
+
+	if (*head == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't pop an empty stack\n", line);
+		/*exit*/
+	}
+
+	*head = new->next;
+	free(new);
+
+	if (*head != NULL)
+		(*head)->prev = NULL;
+}
