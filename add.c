55b79886c8f3998dbdc01d847ef80e599459aafe
nehpets <nehpets0701@gmail.com>
2020-12-23 01:49:07 +0000

    add

---
+++ add.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * add-adds two nodes
+ * @head:input
+ * @line:line args came from
+ */
+void add(stack_t **head, unsigned int line)
+{
+	stack_t *temp = *head, *next;
+
+	if (head == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line);
+		quickExit(*head, EXIT_FAILURE);
+	}
+
+	next = temp->next;
+	next->n += temp->n;
+	free(temp);
+	next->prev = NULL;
+	*head = next;
+}
